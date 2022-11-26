cdf09ec7da5d3e184e97037801b3921a3f4e37fe
tunardev <tunardev@gmail.com>
2022-11-26 11:29:43 +0400

    change code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+
+void solve() {
+    // int n;
+    // cin >> n;
+}
+signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
 }
