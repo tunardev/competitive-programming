ce2e102d4371ed348f8d4806aa0fdf145bbfb101
tunardev <tunardev@gmail.com>
2022-12-02 21:26:59 +0400

    edit code

--- Exam 2/E.cpp
+++ Exam 2/E.cpp
@@ -1,85 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long 
-#define ull long long
+#define ll long long
+#define arr array
+#define ull unsigned long long 
 #define mod (int)(1e9+7)
 
-int main() {
-    int n, m;
-    cin >> n >> m;
-    map<int, int> cnt, cnt2;
-    vector<int> a(n), b(m);
+map<int, int> A, B;
+
+void numbers_factors(map<int, int> &M) {
+    int n;
+    cin >> n;
     for (int i = 0; i < n; ++i) {
-        cin >> a[i];
-        cnt[a[i]]++;
+        int x;
+        cin >> x;
+        for (int j = 2; j*j <= x; j++) {
+            while (x % j == 0) {
+                x /= j;
+                M[j]++;
+            }
+        }
+        if (x > 1) M[x]++;
     }
-    for (int i = 0; i < m; ++i) {
-        cin >> b[i];
-        cnt2[b[i]]++;
-    }
+}
 
-    map<int, int> mp, mp2;
-    for (auto k: cnt) {
-        int x = k.first, y = k.second;
-        for (int i = 2; i * i <= x; ++i) {
-            if (x % i == 0) {
-                while (x % i == 0) {
-                    mp[i] += y;
-                    x /= i;
+int main() {
+    numbers_factors(A);
+    numbers_factors(B);
+
+    ll ans = 1;
+    bool flag = false;
+    for (auto k: A) {
+        if (B.count(k.first)) {
+            int p = k.first;
+            int r = min(A[p], B[p]);
+
+            for (int i = 0; i < r; i++) {
+                ans *= p;
+                if (ans >= (1e9)) {
+                    ans %= (int)(1e9);
+                    flag = true;
                 }
             }
         }
-
-        if (x > 1) {
-            mp[x] += y;
-        }
-    }
-    for (auto k: cnt2) {
-        int x = k.first, y = k.second;
-        for (int i = 2; i * i <= x; ++i) {
-            if (x % i == 0) {
-                while (x % i == 0) {
-                    mp2[i] += y;
-                    x /= i;
-                }
-            }
-        }
-
-        if (x > 1) {
-            mp2[x] += y;
-        }
-    }
-    
-    ull ans = 1;
-    for (auto k: mp) {
-        int x = k.first;
-        int z = mp[x], p = mp2[x];
-        if (min(z, p) == z) {
-            for (int i = 0; i < z; ++i) {
-                ans *= x;
-            } 
-        } else if (min(z, p) == p) {
-            for (int i = 0; i < p; ++i) {
-                ans *= x;
-            }
-        }
     }
 
-    if (ans >= 1e8){        
-        vector<int> r;
-        while (ans) {
-            r.push_back(ans % 10);
-            ans /= 10;
-        }
-        vector<int> p;
-        for (int i = 0; i < 9; ++i) {
-            p.push_back(r[i]);
-        }
-        reverse(p.begin(), p.end());
-        for (int i = 0; i < 9; ++i) {
-            cout << p[i];
-        }
+    if (flag) {
+        cout << setw(9) << setfill('0') << ans;
     } else {
         cout << ans;
     }
